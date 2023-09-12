f9e9d4996bdda0651205c973d22a47d2d1c4e173
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-09-11 22:21:30 -0700

    Create ButterflyPattern.cpp

---
+++ ButterflyPattern.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+   for(int row=0; row<n; row++){
+       for(int col=0; col<row+1; col++){
+           cout << "*";
+       }
+       for(int col=0; col<2*n-2*row-2; col++){
+           cout << " ";
+       }
+       for(int col=0; col<row+1; col++){
+           cout << "*";
+       }
+       
+       cout << endl;
+   }
+   //second half pattern
+  for(int row=0; row<n; row++){
+      for(int col=0; col<n-row-1; col++){
+          cout << "*";
+      }
+      for(int col=0; col<2*row+2; col++){
+          cout << " ";
+      }
+       for(int col=0; col<n-row-1; col++){
+          cout << "*";
+      }
+      cout << endl;
+  }
+   
+
+    return 0;
+}
