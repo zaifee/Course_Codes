6c113b64197d9709e3baf2f111b9bb6b348ecd76
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-10-25 10:11:20 -0700

    Create ShallowcopyandDeepCopy.cpp

---
+++ OOPS/ShallowcopyandDeepCopy.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+// Shallow Copy vs Deep copy 
+using namespace std;
+class abc{
+public:
+int x,*y;
+
+abc(int _x, int _y):x(_x),y(new int(_y)){}
+
+void print() const{
+    printf("X:%d\n PTR Y:%p\n Content of Y(*y): %d\n\n", x, y, *y);
+}
+//Making a deep copy 
+// abc(const abc& obj){
+//     x = obj.x;
+//     y = new int(*obj.y); //it will make a new copy for y now y points to anoter location 
+// }
+    
+    
+};
+
+
+
+int main()
+{
+    abc a{1,2};
+    a.print();
+    
+    abc b = a;
+   
+    b.print();
+    
+    //changing value of y;
+    cout << "Printing the value of b after changing " << endl;
+    *b.y = 20;
+    
+    b.print();
+    
+    cout << "Printing the value of a after changing " << endl;
+    a.print();
+    
+    
+
+    return 0;
+}
