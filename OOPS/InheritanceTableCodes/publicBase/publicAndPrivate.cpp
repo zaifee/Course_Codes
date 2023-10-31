6c8b7bab180c8414d346b14cb24a150b24599841
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-10-31 06:34:43 -0700

    Create publicAndPrivate.cpp

---
+++ OOPS/InheritanceTableCodes/publicBase/publicAndPrivate.cpp
@@ -0,0 +1,33 @@
+
+
+#include <iostream>
+
+using namespace std;
+class Animal{
+public:
+int age;
+void eat(){
+    cout << "Eating " << endl;
+}
+    
+    
+    
+    
+};
+class dog : private Animal{
+    public:
+    void enjoy(){
+        cout << "Enjoying " << endl;
+    }
+};
+
+
+int main()
+{
+    dog d;
+    // d.enjoy(); //runs good 
+    d.eat(); //can't inherited 
+
+    return 0;
+}
+
