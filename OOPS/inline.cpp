b2c493f0c62a9e16740bd107a94723049f03d8f4
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-10-31 01:02:31 -0700

    Create inline.cpp

---
+++ OOPS/inline.cpp
@@ -0,0 +1,35 @@
+
+
+
+#include <iostream>
+
+using namespace std;
+inline void numberShow(int n){
+    cout << n << " ";
+}
+int main()
+{                   
+    numberShow(10);  //compiler this call from this cout << n << endl;
+     numberShow(10);    // cout << n << endl; 
+      numberShow(10);     // cout << n << endl; 
+       numberShow(10);     // cout << n << endl; 
+        numberShow(10);  // cout << n << endl; 
+         numberShow(10);  // cout << n << endl; 
+          numberShow(10);  // cout << n << endl; 
+           numberShow(10);   // cout << n << endl; 
+            numberShow(10);    // cout << n << endl;  
+             numberShow(10);  // cout << n << endl; 
+              numberShow(10);  // cout << n << endl;  
+              
+               numberShow(10);
+                numberShow(10);
+                 numberShow(10);
+                  numberShow(10);
+                   numberShow(10);
+                    numberShow(10);
+                     numberShow(10);
+                      numberShow(10);
+                      
+
+    return 0;
+}
