4113915ccdbfa09d199ff450ce23db326d9f5eaa
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-10-31 06:28:27 -0700

    Create privateAndProtected.cpp

---
+++ OOPS/InheritanceTableCodes/privateBase/privateAndProtected.cpp
@@ -0,0 +1,32 @@
+//private base class and protected derived class 
+
+#include <iostream>
+
+using namespace std;
+class Animal{
+private:
+int age;
+void eat(){
+    cout << "Eating " << endl;
+}
+    
+    
+    
+       
+};
+class dog : protected Animal{
+    public:
+    void enjoy(){
+        cout << "Enjoying " << endl;
+    }
+};
+
+
+int main()
+{
+    dog d;
+    d.enjoy(); //runs good 
+    d.eat(); //can't inherited 
+
+    return 0;
+}
