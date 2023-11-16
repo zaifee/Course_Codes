d1f648ac2620d474935798a4e3b4cc65c4fac7ee
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-11-16 00:03:15 -0800

    Update nextSmallerElement.cpp

--- Stack/nextSmallerElement.cpp
+++ Stack/nextSmallerElement.cpp
@@ -3,34 +3,32 @@
 #include <stack>
 using namespace std;
 
-stack<int>st;
-vector<int>nextSmallerElement(vector<int>&arr){
-st.push(-1);
-vector<int>ans(arr.size());
-for(int i=arr.size()-1; i>=0; i--){
-  int current = arr[i];
-  while(!st.empty() && st.top() >= current){
-    st.pop();
-  }
-  ans[i] = st.empty() ? -1 : st.top();
+stack<int> st;
 
- st.push(current);
+vector<int> nextSmallerElement(vector<int>& arr) {
+    st.push(-1);
+    vector<int> ans(arr.size());
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        int current = arr[i];
+        while (!st.empty() && st.top() >= current) {
+            st.pop();
+        }
+        ans[i] = st.empty() ? -1 : st.top();
 
+        st.push(current);
+    }
+    return ans;
 }
-return ans;
 
-}
 int main() {
-  
-  vector<int>arr{4,1,6,3};
-  
-  vector<int>result(arr.size());
-  result = nextSmallerElement(arr);
+    vector<int> arr{4, 1, 6, 3};
+    vector<int> result(arr.size());
+    result = nextSmallerElement(arr);
 
-  for(auto i : result){
-    cout << i << " ";
-  }cout << endl;
+    for (auto i : result) {
+        cout << i << " ";
+    }
+    cout << endl;
 
-
-  return 0;
+    return 0;
 }
