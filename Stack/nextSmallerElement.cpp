5f0e7aa62653b857c4abcc0bf26a68fab58570c8
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-11-16 00:01:39 -0800

    Create nextSmallerElement.cpp

---
+++ Stack/nextSmallerElement.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+stack<int>st;
+vector<int>nextSmallerElement(vector<int>&arr){
+st.push(-1);
+vector<int>ans(arr.size());
+for(int i=arr.size()-1; i>=0; i--){
+  int current = arr[i];
+  while(!st.empty() && st.top() >= current){
+    st.pop();
+  }
+  ans[i] = st.empty() ? -1 : st.top();
+
+ st.push(current);
+
+}
+return ans;
+
+}
+int main() {
+  
+  vector<int>arr{4,1,6,3};
+  
+  vector<int>result(arr.size());
+  result = nextSmallerElement(arr);
+
+  for(auto i : result){
+    cout << i << " ";
+  }cout << endl;
+
+
+  return 0;
+}
