03161ce76f7cdd9dbb66054259aec55ac04aae6d
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2023-11-19 08:52:06 -0800

    Update hollowRectangle.cpp

--- hollowRectangle.cpp
+++ hollowRectangle.cpp
@@ -1,5 +1,54 @@
 #include <iostream>
 using namespace std;
+int rows = 10;
+int cols = 7;
+
+int main() {
+ for(int i=0; i<rows; i++){
+  for(int j=0; j<cols; j++){
+    if(i == 0 || i == rows-1){
+      cout << "*";  //yaha galti ki thi star ke bad space de diya tha
+    }
+    else{
+      if(j == 0 || j == cols-1){
+        cout << "*";
+      }else{
+        cout << " ";
+      }
+    }
+  }
+  cout << endl;
+ }
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
 *****
 *   *
 *   *
