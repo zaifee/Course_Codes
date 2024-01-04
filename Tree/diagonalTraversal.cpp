34ef9fd8e0584416654fbc9d21fc3396796d435d
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2024-01-03 23:53:25 -0800

    Create diagonalTraversal.cpp

---
+++ Tree/diagonalTraversal.cpp
@@ -0,0 +1,28 @@
+
+
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+
+vector<int> diagonal(Node *root)
+{
+   vector<int>ans;
+   if(!root) return ans;
+   queue<Node*>q;
+   q.push(root);
+   while(!q.empty()){
+       Node* temp = q.front(); q.pop();
+       while(temp != NULL){
+       ans.push_back(temp->data); //root->data nhi push karna temp ka karna hai
+       
+      
+       if(temp->left) q.push(temp->left);
+       temp = temp->right;
+       }
+   }
+   return ans;
+}
