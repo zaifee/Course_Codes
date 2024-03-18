e563b6f5ca6eb80945807c1115ab56a6e0826f83
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2024-03-18 11:15:16 -0700

    Update graphCreationAndTraversal.cpp

--- Graph/graphCreationAndTraversal.cpp
+++ Graph/graphCreationAndTraversal.cpp
@@ -75,39 +75,7 @@ void printEdgeList(){
   }
 }
 
-void bfsTraversal(T src, unordered_map<T, bool> &vis){
-  //adjList already data member me hai 
-  //visited array 
-  
-  //queue
-  queue<T>q;
-  
-  //maintain the inital state 
-  q.push(src);
-  vis[src] = true;
 
-  while(!q.empty()){
-    T frontNode = q.front();
-    cout << frontNode << " ";
-    q.pop();
-
-
-    //go to nbr 
-    for(auto nbr: adjList[frontNode]){
-      T nbrData = nbr.first;
-
-      //check visited or not 
-      if(!vis[nbrData]){
-        q.push(nbrData);
-        vis[nbrData] = true;
-      }
-
-    }
-
-
-  }
-
-}
 
 void dfsTraversal(T src, unordered_map<T, bool>& vis){
   vis[src] = true;
