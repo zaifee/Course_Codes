ec896e90932db4e549a77a19fd025ac2d1685b15
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2024-03-18 11:17:28 -0700

    Update graphCreationAndTraversal.cpp

--- Graph/graphCreationAndTraversal.cpp
+++ Graph/graphCreationAndTraversal.cpp
@@ -76,28 +76,7 @@ void printEdgeList(){
 }
 
 
-
-void dfsTraversal(T src, unordered_map<T, bool>& vis){
-  vis[src] = true;
-  cout << src << " ";
-
-  for(auto nbr : adjList[src]){
-    T nbrData = nbr.first; //bcz we have weighted graph so taking our first means node data
-    if(!vis[nbrData]){
-      dfsTraversal(nbrData, vis);
-    }
-
-  }
-}
-
-
-
-
 };
-
-
-
-
 
 
 int main() {
